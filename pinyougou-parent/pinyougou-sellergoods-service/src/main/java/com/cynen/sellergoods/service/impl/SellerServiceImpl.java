bbc3b93c45556915229fa50329ff9e3a9f5b7426
cynen <cynen@foxmail.com>
2019-11-09 16:30:01 +0800

    添加商家管理的功能.
    商家审核
    商家管理

--- pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/cynen/sellergoods/service/impl/SellerServiceImpl.java
+++ pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/cynen/sellergoods/service/impl/SellerServiceImpl.java
@@ -159,5 +159,13 @@ public class SellerServiceImpl implements SellerService {
 		Page<TbSeller> page= (Page<TbSeller>)sellerMapper.selectByExample(example);		
 		return new PageResult(page.getTotal(), page.getResult());
 	}
+
+		// update商家状态.
+		@Override
+		public void updateStatus(String sellerId, String status) {
+				TbSeller tbSeller = sellerMapper.selectByPrimaryKey(sellerId);
+				tbSeller.setStatus(status);
+				sellerMapper.updateByPrimaryKey(tbSeller);
+		}
 	
 }
