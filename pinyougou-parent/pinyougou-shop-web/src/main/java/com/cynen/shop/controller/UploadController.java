3bd0af83df2468b85b20ffc80e308068671b4759
cynen <cynen@foxmail.com>
2020-02-04 22:40:49 +0800

    新增商品管理的图片上传.
    暂未实现反显

--- pinyougou-parent/pinyougou-shop-web/src/main/java/com/cynen/shop/controller/UploadController.java
+++ pinyougou-parent/pinyougou-shop-web/src/main/java/com/cynen/shop/controller/UploadController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cynen.utils.FastDFSClient;
+
 import entity.Result;
 
 /**
@@ -26,8 +28,22 @@ public class UploadController {
 		
 		//1.获取上传文件的扩展名.
 		String originalFilename = file.getOriginalFilename();
-
-		
+		String extName = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
+		try {
+			// 2.创建一个FastClient客户端.
+			FastDFSClient client = new FastDFSClient("classpath:config/fdfs_client.conf");
+			// 3.上传文件,返回文件的id
+			String fileId = client.uploadFile(file.getBytes(), extName);
+			
+			//4.获得图片的全路径. 
+			String url = FILE_SERVER_URL + fileId;
+			
+			return new Result(true, url);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false, "上传失败!");
+		}
 	}
 	
 }
