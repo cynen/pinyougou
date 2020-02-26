bf774f35b0a385b8b790c225cfaa6894140df1c8
cynen <cynen@foxmail.com>
2020-02-26 23:17:42 +0800

    1、Solr条件查询
    2、搜索页面条件展示。
    3、 分页处理待完善

--- pinyougou-parent/pinyougou-search-service/src/main/java/com/cynen/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-parent/pinyougou-search-service/src/main/java/com/cynen/search/service/impl/ItemSearchServiceImpl.java
@@ -7,12 +7,15 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.FilterQuery;
 import org.springframework.data.solr.core.query.GroupOptions;
 import org.springframework.data.solr.core.query.HighlightOptions;
 import org.springframework.data.solr.core.query.HighlightQuery;
 import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleFilterQuery;
 import org.springframework.data.solr.core.query.SimpleHighlightQuery;
 import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.data.solr.core.query.result.GroupEntry;
@@ -37,6 +40,9 @@ public class ItemSearchServiceImpl implements ItemSearchService{
 	@Autowired
 	private SolrTemplate solrTemplate;
 	
+	@Autowired
+	private RedisTemplate redisTemplate;
+	
 	/**
 	 * 直接搜索solr中的数据，不和mysql数据库进行交互。
 	 */
@@ -58,10 +64,32 @@ public class ItemSearchServiceImpl implements ItemSearchService{
 		// 1.高亮搜索
 		resultMap.putAll(searchList(searchMap));
 		// 2.分类搜索。
-		List categoryList = searchCategoryList(searchMap);
+		List<String> categoryList = searchCategoryList(searchMap);
 		resultMap.put("categoryList", categoryList);
+		if (categoryList.size() > 0) {
+			resultMap.putAll(searchBrandAndSpecList(categoryList.get(0)));
+		}
 		
 		return resultMap;
+	}
+	
+	
+	
+	
+	// 3.查询分类和品牌列表。
+	private Map searchBrandAndSpecList(String category) {
+		Map map = new HashMap();
+		// 获取模板id
+		Long typeId = (Long) redisTemplate.boundHashOps("itemCat").get(category);
+		if(typeId != null ) {
+			// 根据模板id查询品牌列表。
+			List brandList = (List) redisTemplate.boundHashOps("brandList").get(typeId);
+			map.put("brandList", brandList);
+			// 根据模板ID查询规格列表
+			List specList = (List) redisTemplate.boundHashOps("specList").get(typeId);
+			map.put("specList", specList);
+		}
+		return map;
 	}
 	
 	// 2.商品分类列表。
@@ -98,9 +126,52 @@ public class ItemSearchServiceImpl implements ItemSearchService{
 		highlightOptions.setSimplePrefix("<em style='color:red'>");//高亮后缀
 		highlightOptions.setSimplePostfix("</em>");//高亮前缀
 		query.setHighlightOptions(highlightOptions);
-		// 按照关键字查询。
+		
+		
+		// 1.按照关键字查询。
 		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
 		query.addCriteria(criteria );
+		//1.2 商品分类过滤
+		if (!"".equals(searchMap.get("category"))) {
+			FilterQuery filterQuery = new SimpleFilterQuery();
+			Criteria filterCriteria = new Criteria("item_category").is(searchMap.get("category"));
+			filterQuery.addCriteria(filterCriteria);
+			query.addFilterQuery(filterQuery);
+		}
+		
+		//1.3 品牌过滤
+		if (!"".equals(searchMap.get("brand"))) {
+			FilterQuery filterQuery = new SimpleFilterQuery();
+			Criteria filterCriteria = new Criteria("item_brand").is(searchMap.get("brand"));
+			filterQuery.addCriteria(filterCriteria);
+			query.addFilterQuery(filterQuery);
+		}
+		
+		//1.4 按规格过滤
+		if(searchMap.get("spec")!=null){			
+			Map<String,String> specMap= (Map<String, String>) searchMap.get("spec");
+			for(String key :specMap.keySet()){
+				
+				FilterQuery filterQuery=new SimpleFilterQuery();
+				Criteria filterCriteria=new Criteria("item_spec_"+key).is( specMap.get(key)  );
+				filterQuery.addCriteria(filterCriteria);
+				query.addFilterQuery(filterQuery);					
+			}		
+		}
+		
+		//1.6 分页
+		Integer pageNo= (Integer) searchMap.get("pageNo");//获取页码
+		if(pageNo==null){
+			pageNo=1;
+		}
+		Integer pageSize= (Integer) searchMap.get("pageSize");//获取页大小
+		if(pageSize==null){
+			pageSize=20;
+		}
+		
+		query.setOffset( (pageNo-1)*pageSize  );//起始索引
+		query.setRows(pageSize);//每页记录数
+		
 		// 查询出高亮结果的列表。
 		HighlightPage<TbItem> highlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
 		// 循环高亮结果集入口,就是为了将高亮结果集中的内容做高亮展示。
@@ -111,6 +182,8 @@ public class ItemSearchServiceImpl implements ItemSearchService{
 			}
 		}
 		map.put("rows", highlightPage.getContent());
+		map.put("total", highlightPage.getTotalElements());
+		map.put("totalPages", highlightPage.getTotalPages());
 		System.out.println("高亮总记录数： "+highlightPage.getTotalElements());
 		return map;
 	}
