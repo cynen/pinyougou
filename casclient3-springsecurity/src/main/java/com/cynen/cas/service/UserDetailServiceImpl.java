0f77a95eac0e61ced6aaca1586c77e4192e3dcad
cynen <cynen@foxmail.com>
2020-03-09 20:55:19 +0800

    添加SpringSecurity 集成CAS的演示demo

---
+++ casclient3-springsecurity/src/main/java/com/cynen/cas/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cynen.cas.service;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+/**
+ * 认证类，结合CAS
+ * @author myth
+ *
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	/**
+	 * 只做用户的授权，不做具体的校验。
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println(username + "经过了认证类。。。");
+		List <GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		return new User(username, "", authorities );
+	}
+
+}
